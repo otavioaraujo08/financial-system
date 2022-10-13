d7ff665d7f744e59a4cb801470124c1d815ad8a8
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-10-13 11:21:24 -0300

    feat: Header component

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,29 @@
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Sistema Financeiro
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <CreditCardIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
