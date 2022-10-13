f596f20384ffde305cb898c71d516d39f3fd7b36
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-10-13 17:13:00 -0300

    feat: add form component

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,26 @@
 import { Box } from "@mui/material";
 
 import { Cards } from "../../Components/Cards";
+import { Form } from "../../Components/Form";
 import { Header } from "../../Layout/Header";
 
 export function Home() {
   return (
-    <Box style={{ background: "#24293E" }}>
+    <Box
+      style={{
+        background: "#24293E",
+        height: "100vh",
+      }}
+    >
       <Header />
-      <Cards />
+
+      <Box style={{ marginTop: "2rem", marginBottom: "2.5rem" }}>
+        <Cards />
+      </Box>
+
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Form />
+      </Box>
     </Box>
   );
 }
