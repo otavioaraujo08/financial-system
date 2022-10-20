16306afb45a310c43958f6b273011e483a7287b1
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-10-20 11:41:21 -0300

    feat: data utils archive

---
+++ src/Utils/Data.ts
@@ -0,0 +1,22 @@
+import { Item } from "../Types/Item";
+
+export const Data: Item[] = [
+  {
+    date: new Date(2022, 9, 15),
+    category: "Alimentação",
+    title: "Sushizinho",
+    value: 58.9,
+  },
+  {
+    date: new Date(2022, 11, 8),
+    category: "Salário",
+    title: "Decimo terceiro",
+    value: 1300,
+  },
+  {
+    date: new Date(2022, 11, 8),
+    category: "Aluguel",
+    title: "Aluguel",
+    value: 1100,
+  },
+];
