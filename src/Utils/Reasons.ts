d069cbc6dc293ab019e4ccd0339f0722458d15e2
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-10-20 11:42:30 -0300

    fix: reasons to object

--- src/Utils/Reasons.ts
+++ src/Utils/Reasons.ts
@@ -1,9 +1,8 @@
-export const reasons = [
-  "Alimentação",
-  "Aluguel",
-  "Educação",
-  "Lazer",
-  "Salário",
-  "Saúde",
-  "Transporte",
-];
+import { Reasons } from "../Types/Reasons";
+
+export const reasons: Reasons = {
+  food: { title: "Alimentação", color: "#e44c4e", expense: true },
+  rent: { title: "Aluguel", color: "#bc6c25", expense: true },
+  education: { title: "Educação", color: "#ff9f1c", expense: true },
+  salary: { title: "Salário", color: "#4f772d", expense: false },
+};
